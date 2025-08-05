6841ff70d45cf6a9f713d05fb02592df00860795
Jeremy Addy <jeremy.addy@outlook.com>
2025-08-05 12:33:36 -0500

    Adding config to simplify

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
+const config = require('config');
 const express = require('express');
 const app = express();
 
-const PORT = 3000;
-const HOST = '0.0.0.0';
-
-app.listen(PORT, HOST, () => {
-      console.log(`Server running on http://${HOST}:${PORT}`);
+// Start the server
+app.listen(config.port, config.host, () => {
+      console.log(`${config.appName} running on http://${config.host}:${config.port}`);
     });
