93ea050f2588a7b9cb43a380840184cc39431dc9
Jeremy Addy <jeremy.addy@outlook.com>
2025-08-05 12:43:46 -0500

    Expanding config and implimenting scripts

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const config = require('config');
 const express = require('express');
 const app = express();
 
+
+
 // Start the server
 app.listen(config.port, config.host, () => {
       console.log(`${config.appName} running on http://${config.host}:${config.port}`);
